3500706fdfe5a50980adb8e13eb18f8b139e7964
Juan Farias <juanfarias580@gmail.com>
2020-08-23 01:22:36 -0300

    :)

--- crud/app.py
+++ crud/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from .models import db
 from flask_migrate import Migrate
 from .views.index import main
-
+import os
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_pyfile('settings.py')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SECRET_KEY'] = os.urandom(256)
 
     db.init_app(app)
     Migrate(app, db)
