1c8e9f49fe8bc80105248dc6af87dbf14d0d303b
Juan Farias <juanfarias580@gmail.com>
2020-08-23 19:31:07 -0300

    :)

--- crud/app.py
+++ crud/app.py
@@ -7,8 +7,8 @@ import os
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/crud_flask'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.urandom(256)
 
     db.init_app(app)
