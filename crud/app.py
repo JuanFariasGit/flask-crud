3d149717ea9cbd1bb02d4e6a74fe405d9756aaf9
Juan Farias <juanfarias580@gmail.com>
2021-01-09 21:41:31 -0300

    :)

--- crud/app.py
+++ crud/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .models import db
 from flask_migrate import Migrate
 from .views.index import main
-import os
+import secrets
 
 def create_app():
     app = Flask(__name__)
@@ -14,7 +14,7 @@ def create_app():
     db.init_app(app)
     Migrate(app, db)
 
-    app.config['SECRET_KEY'] = os.urandom(256)
+    app.config['SECRET_KEY'] = secrets.token_urlsafe(256)
 
     app.register_blueprint(main)
 
